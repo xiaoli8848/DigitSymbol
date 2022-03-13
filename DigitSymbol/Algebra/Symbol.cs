3e1ec3722c140c780e1181cd51a26bd3910acc63
XiaoLi8848 <45089374+xiaoli8848@users.noreply.github.com>
2022-03-13 19:09:03 +0800

    添加项目文件。

---
+++ DigitSymbol/Algebra/Symbol.cs
@@ -0,0 +1,51 @@
+﻿using NaturplayMath.Algebra.Scalar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitSymbol.Algebra
+{
+    public partial class Symbol
+    {
+        /// <summary>
+        /// 代数符号的字符形式
+        /// </summary>
+        public char Sign { get; set; }
+        /// <summary>
+        /// 数值范围
+        /// </summary>
+        public ValueRange ValueRange { get; set; }
+        /// <summary>
+        /// 对应的数值
+        /// </summary>
+        public Digitable? Value { 
+            get { return Value; } 
+            set { if (!this.ValueRange.isInRange(value)) throw new ArgumentException("给定的数值超出范围。", value.ToString());   Value = value; } 
+        }
+
+        /// <summary>
+        /// 构造一个代数符号，数值范围为任意数。
+        /// </summary>
+        /// <param name="Sign">代数符号的字符形式</param>
+        public Symbol(char sign)
+        {
+            this.Sign = sign;
+            this.ValueRange = ValueRange.ALL;
+        }
+
+        /// <summary>
+        /// 构造一个给定数值范围，且预设对应数值的代数符号。
+        /// </summary>
+        /// <param name="sign">代数符号的字符形式</param>
+        /// <param name="valueRange">数值范围</param>
+        /// <param name="value">对应数值</param>
+        public Symbol(char sign, ValueRange valueRange, Digitable value)
+        {
+            this.Sign = sign;
+            this.ValueRange = valueRange;
+            this.Value = value;
+        }
+    }
+}
---
+++ DigitSymbol/Algebra/ValueRange.cs
@@ -0,0 +1,40 @@
+﻿using NaturplayMath.Algebra.Scalar;
+using NaturplayMath.Algebra.Scalar.NumberString;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitSymbol.Algebra
+{
+    public class ValueRange
+    {
+        public static ValueRange ALL = new ValueRange((num) => { return true; });
+        public static ValueRange ALL_REALNUM = new ValueRange((num) => { return num is RealNum; });
+        public static ValueRange ALL_IRRATIONAL = new ValueRange((num) => { return num is IrrationalNum; });
+        public static ValueRange ALL_RATIONAL = new ValueRange((num) => { return num is RationalNum; });
+        public static ValueRange ALL_NATURALNUM = new ValueRange((num) => { return num is NaturalNumStr || num is IntegerNum && num >= new IntegerNum(); });
+
+        private Func<Digitable, bool> Gudge;
+
+        public bool isInRange(Digitable num)
+        {
+            return Gudge(num);
+        }
+
+        public ValueRange(Func<Digitable, bool> gudge)
+        {
+            this.Gudge = gudge;
+        }
+
+        public static ValueRange FromRealNumRange(Digitable min, Digitable max)
+        {
+            if(max > min)
+            {
+                throw new ArgumentException("限定的数值范围无效。", "[" + min.ToString() + "," + max.ToString() + "]");
+            }
+            return new ValueRange((num) => { return num >= min && num <= max; });
+        }
+    }
+}
